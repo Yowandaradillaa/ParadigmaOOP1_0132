0c953ed3c3112f55e3363a3eae355d9980adf5df
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-20 16:46:46 +0700

    Memasukan akses modifier public

--- Association.cpp
+++ Association.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 class Dokter;
 class Pasien {
-    
-}
+    public :
+    string nama;
+    vector<Dokter*> daftar_dokter;
+    Pasien(string pNama) : nama(pNama){
+        cout << "Pasien \"" << nama << "\" ada \n";
+    }
+};
