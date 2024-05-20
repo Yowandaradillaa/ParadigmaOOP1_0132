3db0456d80c413ea0e29c4c2b436193dad4c1f85
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-20 17:38:34 +0700

    Memasukan class orang dan akses modifier

--- Warisan.cpp
+++ Warisan.cpp
@@ -2,3 +2,13 @@
 #include <string>
 using namespace std;
 
+class Orang {
+    public:
+    string nama;
+    
+    Orang(string pNama) :
+    nama (pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+    
+}
