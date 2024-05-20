27cfac961f33513a2aa52074256adb64876d8299
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-20 11:22:23 +0700

    Memasukan akses modifier public

--- Ibu.h
+++ Ibu.h
@@ -2,6 +2,16 @@
 #define IBU_H
 #include <vector>
 
+#include <iostream>
+#include <string>
+
+
 class Ibu {
-    
+    public:
+    string nama;
+    vector<Anak*> daftar_anak;
+
+    ibu (string pNama) : nama (pNama) {
+        cout << "Ibu \"" << nama << "\" ada \n";
+    }
 }
