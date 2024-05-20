379df5ec2a8baf082baf1902b091d935fc97a57c
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-20 11:16:45 +0700

    Memasukan akses modifier public

--- Anak.h
+++ Anak.h
@@ -1,6 +1,15 @@
 #ifndef ANAK_H
 #define ANAK_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 class anak {
+    public: 
+    string nama;
+    anak (string pNama) : nama(pNama) {
+        cout << "anak\"" << nama << "\" ada \n";
+    }
     
-}
+};
