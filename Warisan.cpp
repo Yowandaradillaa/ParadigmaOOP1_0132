cb61c94dff4826e57b2814a763ba17e3130a548c
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-20 17:47:24 +0700

    Memasukan class pelajar dan function main

--- Warisan.cpp
+++ Warisan.cpp
@@ -17,3 +17,23 @@ class Orang {
         return a + b;
     }
 };
+
+class Pelajar : public orang {
+    public :
+    string sekolah;
+
+    Pelajar(string pNama, string pSekolah) : Orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya" + nama + "dari sekolah" + sekolah + "\n\n";
+    }
+};
+
+int main() {
+    Pelajar siswa1("Andi Lakson", "SMAN 1 BANTUL");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
