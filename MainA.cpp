a44cedadbf5350883f2763e691d0d00c780c6b27
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-20 11:38:06 +0700

    Memasukan function main

--- MainA.cpp
+++ MainA.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
-#using namespace std;
+using namespace std;
 
 #include "Anak.h"
 #include "Ibu.h"
+
+int main () {
+    Ibu* varIbu = new Ibu ("Dini");
+    Ibu* varIbu2 = new Ibu("Novi");
+    Anak* varAnak1 = new Anak("Tono");
+    Anak* varAnak2 = new Anak("Rini");
+    Anak* varAnak3 = new Anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu->tambahAnak(varAnak3);
+    varIbu->tambahAnak(varAnak1);
+}
