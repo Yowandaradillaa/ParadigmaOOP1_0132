a7e3eb9dce9d5517af29cee42adc4e272d76352b
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-20 17:13:41 +0700

    Memasukan header dokter

---
+++ Dokter.h
@@ -0,0 +1,31 @@
+#ifndef DOKTER_H
+#define DOKTER_H
+#include <vector>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Dokter {
+    public:
+    string nama;
+    vector<Dokter*>daftar_dokter;
+
+    Dokter (string pNama) : nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada \n";
+    }
+    ~Dokter (){
+        cout << "Dokter \"" << nama << "\" tidak ada \n";
+    }
+    void tambahPasien(Dokter*);
+    void cetakPasien();
+};
+
+void Dokter::cetakPasien(){
+    cout << "Daftar anak dari ibu\"" << this->nama << "\":\n";
+    for (auto& a :daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+}
+#endif
